34ea94dd60e3c2c04d9aa7e3d301cbcaaf54ea29
Alex <kuznetsov4343@yandex.by>
2025-09-14 02:13:03 +0300

    создание объекта personalMovieDb и заполнение его фильмами и оценками

--- js/index.js
+++ js/index.js
@@ -1,10 +1,22 @@
 "use strict";
 
+let numbersOfFilms;
 
-const names = {
-    name1: 'Alex',
-    name2: 'Bob',
-    name3: 'Max',
-};
+numbersOfFilms = +prompt("сколько фильмов вы уже посмотрели?", "0");
 
-console.log(`your name is ${names.name1}`);
+const personalMovieDb = {
+    count: numbersOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+}
+
+for (let i = 0; i < numbersOfFilms; i++) {
+    let movie = prompt("Один из последних просмотренных фильмов?")
+    let rate = prompt("на сколько его оцените?")
+
+    personalMovieDb.movies[movie] = rate;
+}
+
+console.log(personalMovieDb);
