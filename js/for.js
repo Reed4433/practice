33e0ca0ada604f9e38d6a75effa14ce4d0fdd539
Alex <kuznetsov4343@yandex.cy>
2025-09-18 22:01:40 +0300

    практика в for

--- js/for.js
+++ js/for.js
@@ -1,4 +1,26 @@
 "use strict";
 
+let students = {
+    js: [{
+        name: 'John',
+        progress: 100
+    }, {
+        name: 'Ivan',
+        progress: 60
+    }],
 
+    html: {
+        basic: [{
+            name: 'Peter',
+            progress: 20
+        }, {
+            name: 'Ann',
+            progress: 18
+        }],
 
+        pro: [{
+            name: 'Sam',
+            progress: 10
+        }]
+    }
+};
